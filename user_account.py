d8ed3cd00e5acc1e1cfd895693ceb3ecebf1b270
angelicty <muyenziraissa@gmail.com>
2019-09-06 14:52:03 +0200

    Inserting Credential class inside file

--- user_account.py
+++ user_account.py
@@ -19,3 +19,28 @@ class User:
         save_user method saves user objects into user_holder
         '''
         User.user_holder.append(self)
+
+class Credential:
+    """
+    Class that generates new instances of credential accounts
+    """
+    credential_holder =[]
+
+    def __init__(self,credential_name,username,password):
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            social_media = New credential credential_name
+            credential_username: New credential username.
+            credential_password : New credential password.
+        '''
+        self.social_media= credential_name
+        self.credential_username =username
+        self.credential_password = password
+    def save_credential(self):
+        
+        '''
+        save_credential method saves credential objects into credential_holder
+        '''
+       Credential.credential_holder.append(self)
