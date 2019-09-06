93ca75caa7deb3e9337cec09446aaa4902b9f67d
angelicty <muyenziraissa@gmail.com>
2019-09-06 11:41:31 +0200

    Testing our expected result

--- test_user.py
+++ test_user.py
@@ -2,3 +2,29 @@ import unittest
 from user_account import User
 
 class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the User class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user_account = User("GasaGentille","555a")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user_account.user_username ,"GasaGentille" )
+        self.assertEqual(self.new_user_account.user_password ,"555a" )
+        
+        #SAVING OUR LOGIN DETAILS
+
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
